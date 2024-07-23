f5cc427f7fd32d4886fa5eaf028f66d89cc5d0f5
Bhuvi2308 <kuttybhush@gmail.com>
2024-07-23 23:30:40 +0530

    uploaded pattern program

---
+++ firsttask/src/firsttaskloop/INPUT CLASSES/PatterProgram.java
@@ -0,0 +1,23 @@
+package firsttaskloop;
+
+public class PatterProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=5;i>=1;i--)
+		{
+			for(int j=5;j>i;j--)
+			{
+				System.out.print(j);
+			}
+			
+			for(int k=i;k>=1;k--)
+			{
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
