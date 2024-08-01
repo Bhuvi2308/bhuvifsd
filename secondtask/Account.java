2fc749055af3fe3737b61533ffa1ac5ec99d43d2
Bhuvi2308 <kuttybhush@gmail.com>
2024-08-01 14:18:15 +0530

    output screenshot uploaded

--- secondtask/Account.java
+++ secondtask/Account.java
@@ -1,72 +1,89 @@
 package secondtask;
+import java.util.Scanner;
 
 public class Account {
+private double balance;
+	     
+	    public Account() {
+	        this.balance = 0.0;
+	       
+	    }
+
+	   
+	    public Account(double initialBalance) {
+	        if (initialBalance >= 0) {
+	            this.balance = initialBalance; 
+	        } 
+	    }
+
+	    
+	    public void deposit(double amount) {
+	        if (amount > 0) {
+	            balance += amount;
+	            System.out.println("Deposited: " + amount);
+	        }  
+	    }
+
+	    
+	    public void withdraw(double amount) {
+	        if (amount > 0) {
+	            if (amount <= balance) {
+	                balance -= amount;
+	                System.out.println("Withdrew: " + amount);
+	            } else {
+	                System.out.println("Insufficient balance.");
+	            }
+	        } 
+	    }
+
+	    
+	    public void displayBalance() {
+	        System.out.println("Current balance: " + balance);
+	    }
+	    
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+
+	        
+	        System.out.print("Enter initial balance: ");
+	        double initialBalance = scanner.nextDouble();
+	        Account account = new Account(initialBalance);
+
+	        while (true) {
+	            System.out.println("\nAccount Management System");
+	            System.out.println("1. Deposit");
+	            System.out.println("2. Withdraw");
+	            System.out.println("3. Display Balance");
+	            System.out.println("4. Exit");
+	            System.out.print("Enter your choice: ");
+	            int choice = scanner.nextInt();
+
+	            switch (choice) {
+	                case 1:
+	                    System.out.print("Enter amount to deposit: $");
+	                    double depositAmount = scanner.nextDouble();
+	                    account.deposit(depositAmount);
+	                    break;
+
+	                case 2:
+	                    System.out.print("Enter amount to withdraw: $");
+	                    double withdrawAmount = scanner.nextDouble();
+	                    account.withdraw(withdrawAmount);
+	                    break;
+
+	                case 3:
+	                    account.displayBalance();
+	                    break;
+
+	                case 4:
+	                    System.out.println("Exiting...");
+	                    scanner.close();
+	                    return;
+
+	                default:
+	                    System.out.println("Invalid choice. Please try again.");
+	            }
+	        }
+	    }
+	}
 	
-	private int accountNumber;
-	private double balance;
-	
-	
-	//no argument constructor so the default values will be allocate
-	Account()
-	{
-    accountNumber=0;
-	balance=0.0;	
-	}
-
-	
-	//two argument constructor so the initialize values will be pass here 
-	Account(int accountNumber, double balance) {
-		// TODO Auto-generated constructor stub
-		this.accountNumber=accountNumber;
-		this.balance=balance;
-	}
-
-	
-
-	public int getAccountNumber() {
-		return accountNumber;
-	}
-
-
-	public void setAccountNumber(int accountNumber) {
-		this.accountNumber = accountNumber;
-	}
-
-
-	public double getBalance() {
-		return balance;
-	}
-
-
-	public void setBalance(double balance) {
-		this.balance = balance;
-	}
-
-	// Method to deposit a specified amount into the account
-    public void deposit(double amount) {
-        // Increase the balance by the specified amount
-        balance = balance+ amount;
-    }
-
-    // Method to withdraw a specified amount from the account
-    public void withdraw(double amount) {
-        // Decrease the balance by the specified amount
-        balance = balance- amount;
-    }
-
-    
-    public void balance()
-    {
-    	System.out.println(balance);
-    }
-
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		Account account = new Account(112233,10000);
-		
-		
-		}
-
-}
--- secondtask/Person.java
+++ secondtask/Person.java
@@ -1,45 +1,39 @@
 package secondtask;
 
 public class Person {
-	int age =18;
+    int age;
 	String name;
 	
-	Person(String name)
-	{
-		this.name=name;
-	}
-	  
-	
-	public void display ()
-	{
-		System.out.println("person age is:" + age );
+	   public Person(String name, int i) {
+		// TODO Auto-generated constructor stub
+		if(i==18)
+		{
+		System.out.println("person age is:" + i );
+		}
+		else
+		{
+			System.out.println("person age should be 18");
+		}
+		
 		System.out.println("the person name is:" + name);
-	}
+		}
 
-	public static void main(String[] args) {
+       public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Person person = new Person("bhuvana");
-		person.display();
-	}
+		Person person = new Person("bhuvana",16);}
 
 
 	public int getAge() {
-		return age;
-	}
-
-
+		return age;}
 	public void setAge(int age) {
-		this.age = age;
-	}
-
-
+		this.age = age;}
+	                                 
 	public String getName() {
-		return name;
-	}
+		return name;}
 
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
+    public void setName(String name) {
+	  	this.name = name;}
 }
+	
+
+
