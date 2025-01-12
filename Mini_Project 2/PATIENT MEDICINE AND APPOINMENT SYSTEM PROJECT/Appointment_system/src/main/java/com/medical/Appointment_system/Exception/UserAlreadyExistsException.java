51729df52a80b702109cc3304042416b3b349a87
Bhuvi2308 <kuttybhush@gmail.com>
2025-01-12 11:26:12 +0530

    projects updated
    mini project uploaded

--- Mini_Project 2/PATIENT MEDICINE AND APPOINMENT SYSTEM PROJECT/Appointment_system/src/main/java/com/medical/Appointment_system/Exception/UserAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.medical.Appointment_system.Exception;
-
-public class UserAlreadyExistsException extends RuntimeException{
-
-    public UserAlreadyExistsException(String msg){
-        super(msg);
-    }
-
-}
--- Mini_Project 2/PATIENT MEDICINE AND APPOINMENT SYSTEM PROJECT/Appointment_system/src/main/java/com/medical/Appointment_system/Respository/DoctorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.medical.Appointment_system.Respository;
-
-import com.medical.Appointment_system.Entity.Doctor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DoctorRepository  extends JpaRepository<Doctor,Long> {
-}
